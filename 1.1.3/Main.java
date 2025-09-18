c6ef63b5ffb55d05c01f200e75aa0bb87f856415
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-09-18 19:19:52 +0700

    Update Main.java

--- 1.1.3/Main.java
+++ 1.1.3/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    public static void main(String[] var0) {
-        int var1 = Integer.parseInt(var0[0]);
-        int var2 = Integer.parseInt(var0[1]);
-        int var3 = Integer.parseInt(var0[2]);
-        if (var1 == var2 && var2 == var3) {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        if (a == b && b == c) {
             System.out.println("Equal.");
         } else {
             System.out.println("Not equal.");
