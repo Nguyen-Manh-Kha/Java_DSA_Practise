c254f824122ce8c34951f2b12be86c37050a8565
24021524_Nguyen_Manh_Kha <24021524@vnu.edu.vn>
2025-09-18 18:56:59 +0700

    Create Main.java

---
+++ 1.1.3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] var0) {
+        int var1 = Integer.parseInt(var0[0]);
+        int var2 = Integer.parseInt(var0[1]);
+        int var3 = Integer.parseInt(var0[2]);
+        if (var1 == var2 && var2 == var3) {
+            System.out.println("Equal.");
+        } else {
+            System.out.println("Not equal.");
+        }
+
+    }
+}
